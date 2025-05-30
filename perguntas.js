1172566361eb2c3e347c77285f1734a48cda708c
RonaldFDDS <ronald.sousa@escola.pr.gov.br>
2025-05-30 00:37:23 +0000

    salvar3

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,65 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Moto',
+    'A primeira moto do mundo foi criada quando?',
+    'Em 1885, e ela era inicialmente de madeira.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Moto',
+    'Qual veículo consome menos combustível carro ou moto?',
+    'Motos são mais economicas gastando menos gasolina, fazendo mais quilometragem por litro'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Moto',
+    'Por que o pneu da moto é diferente?',
+    'Para ajudar nas curvas, usando o peso do corpo.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Moto',
+    'Capacete é obrigatório?',
+    'Sim, No Brasil é obrigatório desde 23 de setembro de 1997.'
+)
+
+criaCartao(
+    'Moto',
+    'Alguma moto já foi usada durante a guerra?',
+    'Sim, durante a segunda guerra mundial por alemães para transporte rápido.'
+)
+
+criaCartao(
+    'Moto',
+    'Tem algum lugar no mundo que tem mais motos do que carros?',
+    'Sim, no Vietnã 80% dos veículos são motos.'
+)
+
+criaCartao(
+    'Moto',
+    'Motos eletricas precisam ser emplacadas?',
+    'A partir de 350W ou 25km/h tem que ser emplacada.'
+)
+
+criaCartao(
+    'Moto',
+    'Qual o peso médio de uma moto?',
+    '100 a 200kg.'
+)
+
+criaCartao(
+    'Moto',
+    'Capacete tem validade?',
+    'Sim, normalmente eles tem validade de 5 anos.'
+)
+
+criaCartao(
+    'Moto',
+    'Já teve moto nacional?',
+    'Sim, o Brasil já fabricou uma moto a chamada Amazonas 1600.'
+)
+
+criaCartao(
+    'Moto',
+    'Qual a moto mais rápida do mundo?',
+    'Kawasaki Ninja H2R, atingindo 400km/h.'
 )
